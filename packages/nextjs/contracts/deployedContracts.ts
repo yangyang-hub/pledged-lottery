a34382c63943581760106725b10d789f769d27ea
zjwi3758 <7822575+zjwi3758@user.noreply.gitee.com>
2025-08-02 17:12:37 +0800

    final

--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -7,7 +7,7 @@ import PledgedLotteryAbi from "./abi/PledgedLottery.json";
 const deployedContracts = {
   31337: {
     PledgedLottery: {
-      address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+      address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
       abi: [
         {
           type: "constructor",
